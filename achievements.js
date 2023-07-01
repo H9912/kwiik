ac64993d8e595ffe87d0ab5e237a70c84ed653ab
h912 <77928587+H9912@users.noreply.github.com>
2023-07-01 15:25:24 +0200

    achievements beginning

---
+++ achievements.js
@@ -0,0 +1,21 @@
+
+
+const achievements = [
+    { id: 1, name: 'kiwi newbie', description: 'Collect 10 kiwis', unlocked: false },
+    { id : 2, name: 'kiwi quickly', description: 'buy a press', unlocked: false},
+    { id : 3, name: 'kiwi pushy', description: 'Collect 10k kiwis', unlocked: false},
+    { id: 4, name: 'kiwi lazy', description: 'Buy an extractor', unlocked: false },
+    { id: 5, name: 'kiwi risky', description: 'Unlock the gamble machine', unlocked: false },
+];
+
+
+Game.checkForAchievements = function() {
+    if (Game.kiwis > 10) {
+
+    }
+}
+
+
+Game.openAchievements = function() {
+
+}
